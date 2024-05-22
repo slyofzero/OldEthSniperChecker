71dd07c08d8551403c7c7aec0de7902f949573c0
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-05-22 15:45:12 +0530

    ffixes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,7 +7,7 @@ export const MAESTRO_ROUTER_ADDRESS = "0x80a64c6d7f12c47b7c66c5b4e20e72bc1fcd5d9
 export const NULL_ADDRESS = "0x0000000000000000000000000000000000000000";
 export const ALERT_INTERVAL = 5 * 60;
 export const VOLUME_THRESHOLD = 2e3;
-export const MC_TRACK_THRESHOLD = 24 * 60 * 60;
+export const MC_TRACK_THRESHOLD = 23 * 60 * 60;
 export const TOKEN_ABI = [
   {
     constant: true,
